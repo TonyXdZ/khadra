796075fbed6ae41fc2e9a1e57fc73f1faf17a520
TonyxDz <tony23dz@gmail.com>
2025-07-05 15:50:41 +0100

    feat(users): implement thumbnails with imagekit for profile pics
    - Add 2 thumbnails spec to Profile model 256x256 and 64x64
    - Update profile template to use thumbnail versions with link to original size picture

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from phonenumber_field.modelfields import PhoneNumberField
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
 class Profile(models.Model):
@@ -17,7 +19,22 @@ class Profile(models.Model):
     
     user = models.OneToOneField(User, verbose_name=_('User'), on_delete=models.CASCADE)
     account_type = models.CharField(_('Account type'), max_length=100, choices=ACCOUNT_TYPE_CHOICES, blank=True, null=True)
+    
+    #Original size profile pic
     profile_pic = models.ImageField(_("Profile picture"), default=None, upload_to='profile_pics', blank=True, null=True)
+
+    #Thumbnail of profile pic to be displayed in the profile page
+    profile_pic_256 = ImageSpecField(source='profile_pic',
+        processors=[ResizeToFill(256, 256)],
+        format='JPEG',
+        options={'quality': 100})
+
+    #Thumbnail of profile pic to be displayed in feed and maps page along side the username
+    profile_pic_64 = ImageSpecField(source='profile_pic',
+        processors=[ResizeToFill(64, 64)],
+        format='JPEG',
+        options={'quality': 100})
+
     phone_number = PhoneNumberField(_('Phone number'), help_text=_('Personal phone number'))
     bio = models.TextField(
         _('Biography'),
