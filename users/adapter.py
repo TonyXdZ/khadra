624d392229909be57f29bbd11e6dbbf06188c8a1
TonyxDz <tony23dz@gmail.com>
2025-07-06 20:15:00 +0100

    feat(users): create password change page
    - Add password change template in users/password_change.html
    - Implement redirect to profile page after successful password change in adapter.py

--- users/adapter.py
+++ users/adapter.py
@@ -7,3 +7,11 @@ class KhadraAccountAdapter(DefaultAccountAdapter):
     def get_signup_redirect_url(self, request):
         #redirect users after sign up to profile creation
         return reverse('create-profile')
+
+    def get_password_change_redirect_url(self, request):
+        """
+        The URL to redirect to after a successful password change/set.
+
+        NOTE: Not called during the password reset flow.
+        """
+        return reverse('profile')
