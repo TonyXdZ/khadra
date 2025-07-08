c81ef76198f67f77e6a66e70727e2c7ef71ae8cb
TonyxDz <tony23dz@gmail.com>
2025-07-08 12:16:57 +0100

    feat(users): implement geographic location models
    - Add Country model with name and code fields
    - Add City model with name and foreign key to Country
    - Register both models in users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from users.models import Profile
+from users.models import Profile, Country, City
 
+admin.site.register(Country)
+admin.site.register(City)
 admin.site.register(Profile)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,95 @@
-from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+from django.contrib.gis.gdal import OGRGeometry
+from django.contrib.gis.geos import Point
+from django.db import connection
 from django.utils.translation import gettext as _
 from phonenumber_field.modelfields import PhoneNumberField
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
+
+
+class Country(models.Model):
+    name = models.CharField(_('Name'),max_length=75)
+    iso2 = models.CharField(_('ISO2'), max_length=4)
+    geom = models.MultiPolygonField(_('Geometry'), srid=4326)
+
+    class Meta:
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def lat_lng(self):
+        coords = self.geom.point_on_surface.coords
+        return [coords[1], coords[0]]
+
+
+class City(models.Model):
+
+    name = models.CharField(_('Name'), max_length=75)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, verbose_name=_("Country"))
+    geom = models.MultiPolygonField(_('Geometry'), srid=4326)
+
+    class Meta:
+        verbose_name = _('City')
+        verbose_name_plural = _('Cities')
+
+    def __str__(self):
+        return self.name
+
+    def get_random_location_point(self):
+        """
+        Generate a random Point geometry located within the current model's geometry field (`self.geom`).
+
+        This function uses PostGIS's ST_GeneratePoints to create a single random point (npoints=1)
+        inside the polygon or multipolygon geometry associated with the instance.
+
+        Steps:
+        1. Convert the geometry to EWKT (Extended Well-Known Text) format.
+        2. Use raw SQL to call PostGIS: 
+        ST_GeneratePoints(ST_GeomFromEWKT(...), 1), which returns a MULTIPOINT with 1 random point.
+        3. Convert the result from the database into a Python OGRGeometry.
+        4. Extract the coordinates from the MULTIPOINT and convert it to a GEOS Point object.
+        5. Return this Point, preserving the spatial reference (SRID).
+
+        This can be useful for placing random markers, e.g., assigning a random location inside a city or country boundary.
+        """
+
+        npoints = 1
+        # Prepare sql command, return point as ewkt, passing the
+        # bounding geometry as ewkt.
+        ewkt = "'" + str(self.geom.ewkt) + "'"
+        sql = f'SELECT ST_AsEWKT(ST_GeneratePoints(ST_GeomFromEWKT({ewkt}), {npoints}))'
+        # Instanciate cursor.
+        cursor = connection.cursor()
+        # Inject coords into sql command and execute sql.
+        cursor.execute(sql)
+        row = cursor.fetchall()
+        # Convert result to python geometry.
+        multipoint = OGRGeometry(row[0][0])
+        coords = multipoint.coords[0]
+        srid = multipoint.srid
+        random_point = Point(coords[0], coords[1], srid=srid)
+        return random_point
+
+    def save(self, * args, ** kwargs):
+        """ Setting the country for each city automatically based on it's latitude and longtitude"""
+        point_in_city = self.geom.point_on_surface
+        country = Country.objects.filter(geom__contains=point_in_city)
+        if country.exists():
+            self.country = country.first()
+        else:
+            raise ValueError(_('This city is not contained by any country'))
+        super().save(* args, ** kwargs)
+
+    @property
+    def lat_lng(self):
+        coords = self.geom.point_on_surface.coords
+        return [coords[1], coords[0]]
 
 
 class Profile(models.Model):
