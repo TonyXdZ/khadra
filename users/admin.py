e69bb14d5f38544bb31cb5a556cade14986a1a2c
TonyxDz <tony23dz@gmail.com>
2025-07-08 12:45:07 +0100

    feat(maps): integrate django-leaflet for admin and future user maps

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 from users.models import Profile, Country, City
 
-admin.site.register(Country)
-admin.site.register(City)
+admin.site.register(Country, LeafletGeoAdmin)
+admin.site.register(City, LeafletGeoAdmin)
 admin.site.register(Profile)
