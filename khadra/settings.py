d1a8a57fdfced3e5b28cdd01ccb2e45d74489c4c
TonyxDz <tony23dz@gmail.com>
2025-07-14 19:47:26 +0100

    config: add static files directory

--- khadra/settings.py
+++ khadra/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
 #Media root and url for user uploaded files
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
