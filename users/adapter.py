b9990d89321a94e5912d969da1bab787c9932db9
TonyxDz <tony23dz@gmail.com>
2025-07-15 14:18:46 +0100

    security(users): enforce strict email uniqueness across accounts
    - Added custom email validation in adapter

--- users/adapter.py
+++ users/adapter.py
@@ -1,11 +1,13 @@
 from django.urls import reverse
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from allauth.account.adapter import DefaultAccountAdapter
-
+from allauth.account.models import EmailAddress
 
 class KhadraAccountAdapter(DefaultAccountAdapter):
 
     def get_signup_redirect_url(self, request):
-        #redirect users after sign up to profile creation
+        # Redirect users after sign up to profile creation
         return reverse('create-profile')
 
     def get_password_change_redirect_url(self, request):
@@ -15,3 +17,15 @@ class KhadraAccountAdapter(DefaultAccountAdapter):
         NOTE: Not called during the password reset flow.
         """
         return reverse('profile')
+
+    def validate_unique_email(self, email):
+        """
+        Prevent duplicate emails even if ACCOUNT_UNIQUE_EMAIL fails
+        """
+        if EmailAddress.objects.filter(email=email).exists():
+            raise ValidationError(_("This email is already in use by another account."))
+        return email
+    
+    def clean_email(self, email):
+        email = super().clean_email(email)
+        return self.validate_unique_email(email)
