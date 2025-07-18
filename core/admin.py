8ea28e16d43b9786ab04d59a63edc2e0073d906a
TonyxDz <tony23dz@gmail.com>
2025-07-19 00:24:47 +0100

    feat(core): add Initiative model with volunteer tracking
    - Implement volunteer tracking via ManyToManyField to User
    - Register the model in admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+from core.models import Initiative
 
-# Register your models here.
+admin.site.register(Initiative, LeafletGeoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+from django.utils import timezone
+from django.utils.timezone import now
+from django.utils.translation import gettext as _
+from users.models import City
 
-# Create your models here.
+class Initiative(models.Model):
+
+    STATUS_CHOICES = [
+        ('upcoming', _('Upcoming')),
+        ('ongoing', _('Ongoing')),
+        ('completed', _('Completed')),
+        ('cancelled', _('Cancelled')),
+    ]
+    status = models.CharField(_('Status'), max_length=20, choices=STATUS_CHOICES, default='upcoming')
+
+    info = models.TextField(_('Information'), blank=True, help_text=_('Additional information about the intiative like requests or notes.'))
+    city = models.ForeignKey(City, verbose_name=_('City'), on_delete=models.SET_NULL, null=True, blank=True)
+    geo_location = models.PointField(verbose_name=_('Geolocation'), srid=4326, null=True, blank=True)
+    required_volunteers = models.PositiveIntegerField(_('Required volunteers'))
+    
+    volunteers = models.ManyToManyField(
+        User,
+        verbose_name=_('Volunteers'),
+        related_name='initiatives_joined',
+        blank=True
+    )
+
+    scheduled_datetime = models.DateTimeField(
+        _('Scheduled date and time'), 
+        help_text=_('When is the initiative is going to start')
+    )
+
+    duration_days = models.PositiveIntegerField(
+        _('Duration (days)'),
+        default=1,
+        help_text=_('Number of days the initiative will last')
+        )
+    
+    end_datetime = models.DateTimeField(
+        _('End date and time'),
+        help_text=_('When the initiative ends'),
+        null=True,
+        blank=True
+    )
+    
+    date_created = models.DateTimeField(_('Date created'), default=timezone.now)
+    
+    def __str__(self):
+        return f"initiative {self.pk}"
+
+    class Meta:
+        verbose_name = _('Initiative')
+        verbose_name_plural = _('Initiatives')
+    
+    def get_end_datetime(self):
+        """Calculate end_datetime if not set"""
+        if not self.end_datetime and self.scheduled_datetime:
+            return self.scheduled_datetime + timezone.timedelta(days=self.duration_days)
+        return self.end_datetime
