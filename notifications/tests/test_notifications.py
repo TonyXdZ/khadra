315c7fbd35b479ed059e7e452492de56df888af5
TonyxDz <tony23dz@gmail.com>
2025-08-02 00:12:16 +0100

    test(notifications): Verify manager notification on new initiative creation
    - Test that a single notification is created upon Initiative creation
    - Confirm recipients are managers only (excluding the initiative creator)
    - Ensure users with other account types (volunteer) are not notified
    - Validate the notification is linked to the correct initiative

---
+++ notifications/tests/test_notifications.py
@@ -0,0 +1,89 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import Client, TestCase, override_settings
+from django.urls import reverse
+from django.utils import timezone
+from users.models import Profile, City
+from users.tests.test_utils import create_new_user
+from users.messages import users_messages
+from core.tests.test_utils import create_initiative
+from notifications.models import Notification
+
+UserModel = get_user_model()
+
+# Ovveriding prod spatial data with light weigth test layers to speed up tests
+@override_settings(SPATIAL_LAYER_PATHS=settings.TEST_SPATIAL_LAYER_PATHS)
+class NotificationsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        call_command('load_spatial_layers', 'DZ')
+        self.annaba_city = City.objects.get(name='Annaba')
+        self.point_in_annaba = self.annaba_city.get_random_location_point()
+        self.initiative_creator = create_new_user(email='manager_user@gmail.com',
+                                    username='manager_user',
+                                    password='qsdflkjlkj',
+                                    phone_number='+213555447766', 
+                                    bio='Some good bio',
+                                    account_type='manager',
+                                    city=self.annaba_city,
+                                    geo_location=self.point_in_annaba,
+                                    )
+
+    def test_notification_created_on_initiative_created(self):
+        """
+        Tests that a notification is created and sent to all managers (except the creator)
+        when a new initiative is created.
+        """
+        # Should be notified
+        manager_1 = create_new_user(email='manager_1@gmail.com',
+                                    username='manager_1',
+                                    password='qsdflkjlkj',
+                                    phone_number='+213555447766', 
+                                    bio='Some good bio',
+                                    account_type='manager',
+                                    city=self.annaba_city,
+                                    geo_location=self.point_in_annaba,
+                                    )
+        # Should be notified too    
+        manager_2 = create_new_user(email='manager_2@gmail.com',
+                                    username='manager_2',
+                                    password='qsdflkjlkj',
+                                    phone_number='+213555447766', 
+                                    bio='Some good bio',
+                                    account_type='manager',
+                                    city=self.annaba_city,
+                                    geo_location=self.point_in_annaba,
+                                    )
+
+        # This user should not be in the reciepients
+        volunteer = create_new_user(email='volunteer@gmail.com',
+                                    username='volunteer',
+                                    password='qsdflkjlkj',
+                                    phone_number='+213555447766', 
+                                    bio='Some good bio',
+                                    account_type='volunteer',
+                                    city=self.annaba_city,
+                                    geo_location=self.point_in_annaba,
+                                    )
+        
+        initiative = create_initiative(created_by=self.initiative_creator,
+                                        info="good initiative",
+                                        city=self.annaba_city,
+                                        geo_location=self.point_in_annaba)
+
+        all_notifications = Notification.objects.all()
+        notification = Notification.objects.filter(related_initiative__id=initiative.id).first()
+        manager_1_notified = notification.recipients.contains(manager_1)
+        manager_2_notified = notification.recipients.contains(manager_2)
+        volunteer_not_notified = notification.recipients.contains(volunteer)
+        creator_not_notified = notification.recipients.contains(self.initiative_creator)
+        
+        self.assertEqual(all_notifications.count(), 1)
+        self.assertEqual(notification.recipients.count(), 2) # Only 2 users should be notified
+        self.assertTrue(manager_1_notified)
+        self.assertTrue(manager_2_notified)
+        self.assertFalse(volunteer_not_notified)
+        self.assertFalse(creator_not_notified)
+        
+        
