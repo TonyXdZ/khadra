0f1517136250b8861b00fa92608a4f5064bc7463
TonyxDz <tony23dz@gmail.com>
2025-09-17 19:57:20 +0100

    refactor(notification): switch to GenericForeignKey for related objects

--- notifications/models.py
+++ notifications/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.utils.translation import gettext as _
-from core.models import Initiative
-from users.models import UpgradeRequest
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 User = get_user_model()
 
@@ -16,18 +16,15 @@ class Notification(models.Model):
         ('initiative_started', _('Initiative Started')),
         ('initiative_cancelled', _('Initiative Cancelled')),
         ('initiative_completed', _('Initiative Completed')),
-        # Announcement is for notifying all users with news
-        # or with events unrelated to any initiative or specific user
-        # this will come in handy in the future to communicate with all users
+        # Announcement (for all users, no related object required)
         ('announcement', _('Announcement')),
-        # Users related notifictions categories
+        # Users related notifications categories
         ('upgrade_request_created', _('Upgrade Request Created')),
     ]
 
     notification_type = models.CharField(_('Notification type'), max_length=50, choices=NOTIFICATION_TYPES)
     message = models.TextField(_('Message')) 
     
-    # Recipients: Many-to-Many so we can target one, many, or zero users
     recipients = models.ManyToManyField(
         User,
         blank=True,
@@ -35,19 +32,17 @@ class Notification(models.Model):
         verbose_name=_('Recipients'),
     )
 
-    is_broadcast = models.BooleanField(_('Is broadcast'), default=False, 
-                    help_text=_('If set to True the notification will be to all users.'))
-    
-    # Link the notification to the relevant Initiative
-    related_initiative = models.ForeignKey(Initiative, on_delete=models.CASCADE, 
-                                            null=True, blank=True, related_name='notifications',
-                                            verbose_name=_('Related initiative'))
+    is_broadcast = models.BooleanField(
+        _('Is broadcast'),
+        default=False,
+        help_text=_('If set to True the notification will be sent to all users.'),
+    )
 
-    # Link the notification to the relevant Upgrade Request
-    related_upgrade_request = models.ForeignKey(UpgradeRequest, on_delete=models.CASCADE, 
-                                            null=True, blank=True, related_name='notifications',
-                                            verbose_name=_('Related upgrade request'))
-    
+    # --- Generic relation fields ---
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    related_object = GenericForeignKey('content_type', 'object_id')
+
     # Status
     is_read = models.BooleanField(_('Is read'), default=False)
     created_at = models.DateTimeField(_('Created at'), default=timezone.now)
@@ -58,9 +53,8 @@ class Notification(models.Model):
         verbose_name_plural = _('Notifications')
 
     def __str__(self):
-        return f'Notification {self.pk}'
+        return f'Notification {self.pk} - {self.notification_type}'
 
-    # Helper method to mark notification as read
     def mark_as_read(self):
         if not self.is_read:
             self.is_read = True
